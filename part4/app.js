bdc94dd517592b5ec6a491c77a76237bc4512443
Jack Tsai <jtsai@nexify.com.hk>
2022-09-30 17:58:40 +0800

    update part4

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const mongoose = require('mongoose')
 mongoose.connect(config.MONGODB_URI)
@@ -20,6 +21,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
