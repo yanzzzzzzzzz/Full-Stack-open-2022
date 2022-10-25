5fb5f4ee008e883e5f3b78c6f7d27e4455e5ca5d
Jack Tsai <jtsai@nexify.com.hk>
2022-10-25 15:16:32 +0800

    clean code

--- part2/countries/App.js
+++ part2/countries/App.js
@@ -1,49 +1,48 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import CountriesList from './components/CountriesList'
-import CountryInfo from './components/CountryInfo'
+import CountriesList from "./components/CountriesList";
+import CountryInfo from "./components/CountryInfo";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
   const [countriesToShow, setCountriesToShow] = useState([]);
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
-      setCountries(response.data)
-      setCountriesToShow(response.data)
-    })
-  }, [])
+      setCountries(response.data);
+      setCountriesToShow(response.data);
+    });
+  }, []);
 
-  const filterListener = (event) =>{
-    const search = event.target.value
-    setFilter(search)
+  const filterListener = (event) => {
+    const search = event.target.value;
+    setFilter(search);
     const countriesFilterData = countries.filter((country) =>
-      country.name.common.toLowerCase().includes(search.toLowerCase()))
-    setCountriesToShow(countriesFilterData)
-  }
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
+    setCountriesToShow(countriesFilterData);
+  };
   return (
     <div>
-      find countries <input value={filter} onChange={filterListener}/>
-      {countriesToShow.length === 0 
-        ? (<p>The countries that don't exist</p>)
-        : null
-      }
-      {countriesToShow.length === 1
-        ? (<CountryInfo country={countriesToShow[0]} /> )
-        : null
-      }
-      {countriesToShow.length > 10 
-        ? (<p>Too many matches, specify another filter</p>) 
-        : (<CountriesList 
-            countriesToShow={countriesToShow}
-            setCountriesToShow={setCountriesToShow}
-            />
-          )
-      }
+      find countries <input value={filter} onChange={filterListener} />
+      {countriesToShow.length === 0 ? (
+        <p>The countries that don't exist</p>
+      ) : null}
+      {countriesToShow.length === 1 ? (
+        <CountryInfo country={countriesToShow[0]} />
+      ) : null}
+      {countriesToShow.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : (
+        <CountriesList
+          countriesToShow={countriesToShow}
+          setCountriesToShow={setCountriesToShow}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
